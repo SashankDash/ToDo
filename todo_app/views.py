8251ea60aede6f9d994b938bac48f87d007f879e
Sashank <sashanksekharbapun@gmail.com>
2020-07-18 15:01:17 +0530

    Alert added to html

--- todo_app/views.py
+++ todo_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,HttpResponse
 from .models import Task
 # Create your views here.
 def home (request):
-    sucess = 'False'
+    context = {'sucess' : False}
     if request.method == "POST":
         title = request.POST['title']
         desc = request.POST['desc']
@@ -10,10 +10,10 @@ def home (request):
         ins =Task(taskTitle =title, taskDesc = desc)
         ins.save()
 
-        sucess = 'True'
+        context = {'sucess' : True}
         
     
-    return render(request,'todo_app/index.html',{'sucess':sucess})
+    return render(request,'todo_app/index.html',context)
 
 def task (request):
     return render(request,'todo_app/task.html')
